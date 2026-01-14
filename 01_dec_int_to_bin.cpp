7a96384220f26ae2ff8fdd995d46943a37d41f45
Mishhhkan <159691363+mishhhkan@users.noreply.github.com>
2026-01-14 14:30:10 +0300

    Add files via upload

---
+++ 01_dec_int_to_bin.cpp
@@ -0,0 +1,41 @@
+/*
+Программа: Перевод целого числа из десятичной системы в двоичную
+Раздел методички: Раздел 1. Перевод целых чисел из 10СС в 2СС
+Описание: Реализует перевод положительного целого числа из 10-й СС в 2-ю СС с дополнением нулями до заданной разрядности
+*/
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    long long num;
+    int bits = 8; // можно изменить
+
+    cout << "Введите целое положительное число в 10 СС: ";
+    cin >> num;
+
+    if (num == 0) {
+        cout << "В 2 СС: 0" << endl;
+        return 0;
+    }
+
+    string bin = "";
+    long long temp = num;
+    while (temp > 0) {
+        bin += (temp % 2 == 0 ? '0' : '1');
+        temp /= 2;
+    }
+    reverse(bin.begin(), bin.end());
+
+    cout << "В 2 СС: " << bin << endl;
+
+    string padded = string(bits - bin.length(), '0') + bin;
+    if (bin.length() < bits) {
+        cout << "Дополнено до " << bits << " бит: " << padded << endl;
+    }
+
+    return 0;
+}
