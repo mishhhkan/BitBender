14fb0fbeb13231aea5a1fcfc0ff125e7705f86b2
Mishhhkan <159691363+mishhhkan@users.noreply.github.com>
2026-01-21 21:13:45 +0300

    Update 02_decimal_fractional_to_binary.cpp

--- 02_decimal_fractional_to_binary.cpp
+++ 02_decimal_fractional_to_binary.cpp
@@ -1,51 +1,76 @@
 /*
-Программа: Перевод числа с плавающей точкой из десятичной в двоичную
-Раздел методички: Раздел 2. Перевод чисел с плавающей точкой из 10СС в 2СС
-Описание: Отдельно переводит целую и дробную части в двоичное представление
+Программа: Перевод дробного числа из десятичной системы в двоичную
+Раздел методического пособия: Раздел 2. Перевод чисел с плавающей точкой из 10СС в 2СС
+Описание: Реализует перевод положительного дробного числа из 10-й СС в 2-ю СС с подробным выводом шагов
 */
 
-#include <iostream>
-#include <string>
-#include <algorithm>
 
-using namespace std;
+#include <iostream> // Библиотека для ввода-вывода
 
-string int_to_bin(long long n) {
-    if (n == 0) return "0";
-    string res = "";
-    while (n > 0) {
-        res += (n % 2 == 0 ? '0' : '1');
-        n /= 2;
-    }
-    reverse(res.begin(), res.end());
-    return res;
-}
 
-string frac_to_bin(double f, int max_prec = 20) {
-    string res = "";
-    while (f > 0 && res.length() < max_prec) {
-        f *= 2;
-        int bit = (f >= 1.0 ? 1 : 0);
-        res += ('0' + bit);
-        if (bit) f -= 1.0;
-    }
-    return res;
-}
+using namespace std; // Пространство имен std
+
 
 int main() {
-    double num;
-    cout << "Введите положительное число с плавающей точкой в 10 СС: ";
-    cin >> num;
+    // Заголовок и ввод данных
+    cout << "=== Программа перевода числа с плавающей точкой из 10 СС в 2 СС ===\n";
+    cout << "Введите дробное число от 0 до 255 в 10 СС для перевода (например 10.5): ";
+    float number; // Исходное число в 10 СС
+    cin >> number; // Ввод исходного числа в 10 СС
 
-    if (num < 0) num = -num; // знак игнорируем для примера
+    // Разделение на целую и дробную части
+    int integer_part = (int) number; // Целая часть числа
+    float fractional_part = number - integer_part; // Дробная часть числа
 
-    long long int_part = (long long) num;
-    double frac_part = num - int_part;
+    cout << "Перевод числа " << number << " из 10 СС в 2 СС:\n";
+    cout << "Целая часть: " << integer_part << "\n";
+    cout << "Дробная часть: " << fractional_part << "\n";
 
-    string bin_int = int_to_bin(int_part);
-    string bin_frac = frac_to_bin(frac_part);
+    // Проверка на нулевое число (0 в 10 СС = 0 в 2 СС)
+    if (number == 0.0) {
+        cout << "Результат перевода: 0\n";
+        return 0;
+    }
 
-    cout << "В 2 СС: " << (bin_int == "0" ? "0" : bin_int) << "." << bin_frac << endl;
+    // Перевод целой части
+    string integer_bin; // Бинарное представление целой части
+    int step = 1; // Счетчик шагов для целой части
 
+    if (integer_part == 0) {
+        integer_bin = "0";
+    } else {
+        int num = integer_part;
+        while (num > 0) {
+            int ost = num % 2; // Вычисление остатка от деления на 2
+            integer_bin = to_string(ost) + integer_bin; // Добавление остатка в начало строки
+            cout << "Шаг " << step << " (целая часть): число = " << num
+                    << ", остаток = " << ost << '\n';
+            num = num / 2; // Целочисленное деление на 2
+            step = step + 1; // Увеличение счетчика шагов
+        }
+    }
+
+    // Перевод дробной части
+    string fractional_bin; // Строка для хранения двоичного представления дробной части
+    int step_frac = 1; // Счетчик шагов для дробной части
+
+    if (fractional_part > 0.0) {
+        cout << "\nПеревод дробной части:\n";
+        while (fractional_part > 0.0) {
+            cout << "Шаг " << step_frac << " (дробная часть): дробь = " << fractional_part
+                    << ", умножить на 2 = " << fractional_part * 2.0 << '\n';
+            fractional_part = fractional_part * 2.0; // Умножение дробной части на 2
+            int bit = (int) fractional_part; // Извлечение целой части результата умножения
+            cout << "Целая часть (бит) = " << bit << '\n';
+            fractional_bin = fractional_bin + to_string(bit); // Добавление бита в конец строки
+            fractional_part = fractional_part - bit; // Обновление дробной части
+            step_frac = step_frac + 1; // Увеличение счетчика шагов
+        }
+    }
+
+    // Сборка результата
+    string result = integer_bin + "." + fractional_bin;
+
+    cout << "Результат перевода: " << result << '\n';
     return 0;
 }
